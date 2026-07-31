f458c0d4e7b200dcaf784480567971ebb072fbe0
mmibarra5 <michell.ibarra_santillana@outlook.com>
2026-07-30 22:29:34 -0500

    feat: agrega entidad producto, repositorio jpa y siembra de cafes

--- src/main/java/ec/edu/espe/agrosmart/AgrosmartApplication.java
+++ src/main/java/ec/edu/espe/agrosmart/AgrosmartApplication.java
@@ -1,7 +1,13 @@
 package ec.edu.espe.agrosmart;
 
+import ec.edu.espe.agrosmart.entity.ProductoEntity;
+import ec.edu.espe.agrosmart.repository.ProductoRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import java.math.BigDecimal;
+import java.util.List;
 
 @SpringBootApplication
 public class AgrosmartApplication {
@@ -10,4 +16,28 @@ public class AgrosmartApplication {
 		SpringApplication.run(AgrosmartApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner sembrarCafes(ProductoRepository repository) {
+		return args -> {
+			if (repository.count() == 0) {
+				repository.saveAll(List.of(
+						new ProductoEntity("Café Arábica Lavado Alta Montaña",
+								new BigDecimal("12.50"), 100, "Café",
+								"compras@cafeteria.ec,barista@cafeteria.ec"),
+						new ProductoEntity("Café Geisha Natural Microlote",
+								new BigDecimal("28.00"), 50, "Café",
+								"ventas@especialidad.ec"),
+						new ProductoEntity("Café Honey Process Origen Único",
+								new BigDecimal("18.75"), 80, "Café",
+								"pedidos@cafeteria.ec"),
+						new ProductoEntity("Café Sin Precio Asignado",
+								new BigDecimal("0.00"), 40, "Café",
+								"info@cafeteria.ec"),
+						new ProductoEntity("Café Sin Correos de Notificación",
+								new BigDecimal("15.00"), 60, "Café",
+								"")
+				));
+			}
+		};
+	}
 }
